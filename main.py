4934ef2247acf8bb1033b57abe47717738378887
Fredohm <fredwaucquez@hotmail.fr>
2022-01-28 17:03:32 +0100

    create classes and init screen

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # Pong Game
+from turtle import Screen
+# create screen
+screen = Screen()
+
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+
+# create two paddles
+
+# create the ball and make it move
+
+# detect collision with the wall and bounce
+
+# detect collision with paddle
+
+# detect when paddle misses
+
+# keep score
+
+screen.exitonclick()
