d9a9a830f19c74bdc01360b8a355f89c2782a94d
Fredohm <fredwaucquez@hotmail.fr>
2022-01-29 15:53:31 +0100

    create paddles

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 # Pong Game
-from turtle import Screen
+from turtle import Turtle, Screen
+from paddle import Paddle
+
+WIDTH = 800
+HEIGHT = 600
+
+LEFT_PADDLE_STARTING_POS = [(-360, -20), (-360, 0), (-360, 20)]
+RIGHT_PADDLE_STARTING_POS = [(360, -20), (360, 0), (360, 20)]
+
 # create screen
 screen = Screen()
 
-screen.setup(width=800, height=600)
+screen.setup(width=WIDTH, height=HEIGHT)
 screen.bgcolor("black")
 screen.title("Pong")
 screen.tracer(0)
 
+# Draw dashed_line in the middle of the screen
+net = Turtle("square")
+net.color("white")
+net.pensize(10)
+net.penup()
+net.sety(-WIDTH / 2)
+
+while net.ycor() < WIDTH / 2:
+    net.pendown()
+    net.setheading(90)
+    net.forward(40)
+    net.penup()
+    net.forward(40)
+
 
 # create two paddles
+left_paddle = Paddle()
+left_paddle.create_paddle(LEFT_PADDLE_STARTING_POS)
+
+right_paddle = Paddle()
+right_paddle.create_paddle(RIGHT_PADDLE_STARTING_POS)
+screen.update()
 
 # create the ball and make it move
 
--- paddle.py
+++ paddle.py
@@ -1,1 +1,16 @@
-# create and move a paddle
+from turtle import Turtle
+
+
+class Paddle:
+
+    def __init__(self):
+        self.segments = []
+        self.create_paddle([])
+
+    def create_paddle(self, positions):
+        for position in positions:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
